1dfe7f969101322bfd6e415fce35c219eddfc249
webhjs <49713315+webhjs@users.noreply.github.com>
2021-05-07 10:36:21 +0800

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 金苏
+ * @Date: 2021-04-06 09:08:22
+ * @LastEditors: 金苏
+ * @LastEditTime: 2021-05-07 10:33:55
+ */
+export const routerFile = "simple" // 路由配置
