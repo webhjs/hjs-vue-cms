3ed484a03d5a9a8ca7b67bbb63a0c189c81a9d14
huangjs <huangjs@ai-english.com.cn>
2021-08-02 15:51:18 +0800

    新增流程图

--- src/views/simple-template/jsplumb/index.vue
+++ src/views/simple-template/jsplumb/index.vue
@@ -4,12 +4,12 @@
  * @Author: 金苏
  * @Date: 2021-07-14 14:14:22
  * @LastEditors: 金苏
- * @LastEditTime: 2021-08-02 15:11:43
+ * @LastEditTime: 2021-08-02 15:49:46
 -->
 <template>
   <div class="containers flex flex-col">
     <Jsplumbdtarget id="js2" />
-    <!-- <JsplumbdtargetView id="js1" /> -->
+    <JsplumbdtargetView id="js1" />
     <Jsplumbdrag id="js3" />
     <Jsplumblist :sourceList="sourceList" :targetList="targetList" :relationArray="relationArray" />
   </div>
