c010913c3d25827d4d7a275826c02d2330bff52c
奕天 <1395516797@qq.com>
2022-10-12 10:46:30 +0800

    提交

--- src/libs/utils/custom.js
+++ src/libs/utils/custom.js
@@ -16,6 +16,8 @@ function guid() {
 }
 
 function dateFormat(fmt, date) {
+  if(!date) return
+  date = new Date(date)
   let ret;
   const opt = {
     "Y+": date.getFullYear().toString(),        // 年
@@ -44,4 +46,114 @@ function isJSON(str) {
   }
 }
 
-export { guid, dateFormat, isJSON }
+
+
+//数字对应汉子转换
+function numberENFun(num) {
+  const numberCN = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十'];
+  let string = num.toString();
+  let resultStr = '';
+  string.split('').forEach(function (item, i) {
+    if (i == 1) {
+      if (item * 1 != 0) {
+        resultStr += '十' + numberCN[item * 1 - 1]
+      } else {
+        resultStr += '十'
+      }
+    } else {
+      resultStr += numberCN[item * 1 - 1]
+    }
+  })
+  if (resultStr == '一十') {
+    resultStr = '十';
+  }
+  return resultStr;
+}
+
+function converter(value) {
+  // A-Z计数器转换器
+  const type = typeof value;
+  let finaly = null;
+  switch (type) {
+    case "number":
+      finaly = "";
+      let divisor = Math.floor(value / 26), remainder = value % 26 ? [value % 26] : value <= 26 ? [value] : [];
+      while (divisor > 26) {
+        divisor = Math.floor(divisor / 26);
+        remainder.unshift(divisor % 26);
+      }
+      value > 26 && remainder.unshift(divisor);
+      for (let val of remainder) {
+        finaly += String.fromCharCode(val + 64);
+      }
+      break;
+    case "string":
+      finaly = 0;
+      const length = value.length;
+      for (let len = 0; len < length; len++) {
+        finaly +=
+          (value.charAt(len).charCodeAt() - 64) *
+          Math.pow(26, length - len - 1);
+      }
+      break;
+  }
+  return finaly;
+}
+
+function clickTitle(mark) {
+  document.querySelector(`#${mark.questioncode}`).scrollIntoView(true)
+}
+
+// 异步加载路由给路由配置routename
+const Lazy = async function (component = new Promise(resolve => resolve), componentName) {
+  const _component = await component
+  componentName && (_component.default.name = componentName)
+  return Promise.resolve(_component)
+}
+
+/*
+ * 根据身份证获取出生日期
+ * */
+function idCardParse(cardNum = '') {
+  // 获取输入身份证号码
+  //获取出生日期
+  const birthDate = cardNum.substring(6, 10) + '-' + cardNum.substring(10, 12) + '-' + cardNum.substring(12, 14)
+  let sex = 2 // 默认 女
+  // 获取性别
+  if (parseInt(cardNum.substr(16, 1)) % 2 == 1) {
+    sex = 1
+  }
+  //获取年龄
+  let myDate = new Date()
+  let month = myDate.getMonth() + 1
+  let day = myDate.getDate()
+  let age = myDate.getFullYear() - cardNum.substring(6, 10) - 1
+  if (cardNum.substring(10, 12) < month || (cardNum.substring(10, 12) == month && cardNum.substring(12, 14) <= day)) {
+    age++
+  }
+  //年龄 age
+  return {
+    birthDate,
+    sex,
+    age
+  }
+}
+
+function requireContext(modulesFiles, name) {
+  const apiMap = modulesFiles.keys().reduce((modules, modulePath) => {
+    const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+    const value = modulesFiles(modulePath)
+    modules[moduleName] = name ? value[name] : value
+    return modules
+  }, {})
+  return apiMap
+}
+
+export default function to(promise) {
+  return promise.then(data => {
+     return [null, data]; //数组大小为2，第一项用null占位置，也达到了 !err == true 的效果
+  })
+  .catch(err => [err]); // 数组大小为1，根本没有第二项
+}
+
+export { guid, requireContext, dateFormat, isJSON, converter, numberENFun, clickTitle, idCardParse, Lazy, to }
---
+++ src/router/common.js
@@ -0,0 +1,43 @@
+import Login from "@/views/login";
+import Layout from "@/libs/layout/Layout";
+import { Lazy } from '@/libs/utils/custom'
+
+let routerFile = ''
+const hash = /.*?#.*?/.test(window.location.href)
+if (hash) {
+    routerFile = window.location.hash.match(/#\/([\w-]*)\/?/)[1]
+} else {
+    routerFile = window.location.pathname.match(/\/([\w-]*)\/?/)[1]
+}
+export const nameSpace = routerFile;
+
+export const constantCommonRouterMap = [
+  {
+    path: "/redirect",
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: "/redirect/:path(.*)",
+        component: resolve => require(["@/views/redirect"], resolve)
+      }
+    ]
+  },
+  {
+    path: "/login",
+    name: "login",
+    hidden: true,
+    component: Login,
+    meta: {
+      title: "登录"
+    }
+  },
+  {
+    path: "/404",
+    component: () => Lazy(import("@/views/error-page/404")),
+    hidden: true
+  }
+]
+export const asyncCommonRouterMap = [
+    { name: '404', path: "*", redirect: "/404", hidden: true }
+]
